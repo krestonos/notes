12ceedd0da676b366591290cbc920d92f1ddb883
Dante <a.bavyka@netpeak.net>
2017-12-02 16:14:30 +0200

    add actions and views

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Note;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -13,9 +15,48 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        $em = $this->getDoctrine()->getManager();
+        $notesRepo = $em->getRepository(Note::class);
+        $allNotes = $notesRepo->findAll();
+        return $this->render('@App/index.html.twig', [
+            'notes' => $allNotes,
         ]);
+    }
+    
+    /**
+     * @Route("/add")
+     */
+    public function addAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createFormBuilder()
+            ->add('text', TextType::class)
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $newNote = new Note();
+            $formData = $form->getData();
+            $newNote->setText($formData['text']);
+            $em->persist($newNote);
+            $em->flush();
+        }
+        return $this->render('@App/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    
+    /**
+     * @Route("/show/{id}")
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $notesRepo = $em->getRepository(Note::class);
+        $note = $notesRepo->find($id);
+        return $this->render('@App/show.html.twig', [
+            'note' => $note,
+        ]);
+        
+    
     }
 }
